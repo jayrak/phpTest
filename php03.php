cd59460bf06350ee3fdf29fc6066775c707cf64c
jayrak <jay001year@gmail.com>
2020-09-07 16:08:53 +0900

    project3fix

--- php03.php
+++ php03.php
@@ -6,7 +6,7 @@ function multiplier($number){
     return $number;
 }
 echo multiplier(50);
-echo "\n<br>";
+echo "\n";
 
 /*2.$a と $b を仮引数に持ち、　$a と $b　を足した結果を返す関数を作成してください。
 　参考）引数は下記のように指定すると、複数の仮引数を使うことができます。*/
@@ -16,7 +16,7 @@ function f($a, $b){
     return $plus;
 }
 echo f(5,8);
-echo "\n<br>";
+echo "\n";
 
 /*3.$arr という配列の仮引数を持ち、
 数値が入った配列array(1, 3, 5 ,7, 9) を渡すとその要素をすべてかけた結果を返す関数を作成してください。*/
@@ -28,7 +28,7 @@ function multipliers($arr){
     return $total;
 }
 echo multipliers(array(1,3,5,7,9));
-echo "\n<br>";
+echo "\n";
 
 /*4.【応用】　次のプログラムは、配列の中で1番大きい値を返す max_array という関数を実装しようとしています。
 途中の部分を完成させてください。*/
@@ -42,35 +42,35 @@ function max_array($arr) {
     return $max_number;
 }
 echo max_array(array(1,3,5,7,9));
-echo "\n<br>";
+echo "\n";
 
 //5.次のビルトイン関数の用途、使い方を自分で調べて実際に使ってみてください。
 //strip_tags
 $text="<h1>はるか昔、世界は無に染められていた。</h1>";
 echo $text;
 echo strip_tags($text);
-echo "\n<br>";
+echo "\n";
 
 //array_push
 $arrold = array('1','2','3');
 array_push($arrold,'4','5','6');
 print_r ($arrold);
-echo "\n<br>";
+echo "\n";
 
 //array_merge
 $arrold = array('1','2','3');
 $arrfix = array('4','5','6');
 $result = array_merge($arrold,$arrfix);
 print_r($result);
-echo "\n<br>";
+echo "\n";
 
 //time, mktime
 echo '現在のUnixタイムスタンプ：'.time();
-echo "\n<br>";
+echo "\n";
 
 //mktime( [ 時 [, 分 [, 秒 [, 月 [, 日 [, 年 [, int is_dst ]]]]]]] )
 echo '指定のUnixタイムスタンプ：'.mktime(16, 4, 50, 9, 7, 2020);
-echo "\n<br>";
+echo "\n";
 
 //date
 $week = [
